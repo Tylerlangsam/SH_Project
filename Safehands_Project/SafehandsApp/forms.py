583818aed1ba8e61ff647fee8c6f5b0109ab61ed
Tylerlangsam <90009414+Tylerlangsam@users.noreply.github.com>
2021-12-30 10:12:54 -0800

    Adding Comments

--- Safehands_Project/SafehandsApp/forms.py
+++ Safehands_Project/SafehandsApp/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from .models import Babysitter, Child
-
+# Child form allowing user to input data for a Child Profile
 class ChildForm(forms.Form):
     name = forms.CharField(max_length=25, required=True)
     age = forms.CharField(max_length=25, required=True)
@@ -9,7 +9,7 @@ class ChildForm(forms.Form):
     for babysitter in Babysitter.objects.all():
         choices.append((babysitter.babysitter_id, babysitter.name + ", " + babysitter.gender + ", " + babysitter.age ))
     babysitters = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple, choices=choices, required=True)
-
+# Report form allowing user to input data to create a report for a specific Child
 class ReportForm(forms.Form):
     meal = forms.CharField(max_length=25, required=True)
     potty = forms.CharField(max_length=25, required=True)
@@ -18,7 +18,7 @@ class ReportForm(forms.Form):
     for child in Child.objects.all():
         choices.append((child.child_id, child.name))
     child = forms.ChoiceField(widget=forms.Select, choices=choices, required=True)
-
+# Babysitter for allowing user to create a profile for a babysitter
 class BabysitterForm(forms.Form):
     name = forms.CharField(max_length=25, required=True)
     age = forms.CharField(max_length=25, required=True)
--- Safehands_Project/SafehandsApp/models.py
+++ Safehands_Project/SafehandsApp/models.py
@@ -2,20 +2,26 @@ from django.db import models
 
 # Create your models here.
 
+# A model for the Babysitter profiles
 class Babysitter(models.Model):
+# This model takes 3 inputs, name age and gender
     babysitter_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
     age = models.CharField(max_length=2)
     gender = models.CharField(max_length=20)
 
+# A model for the Child profiles
 class Child(models.Model):
+# This model takes 4 inputs, name, age, gender, and a many to many field from the Babysitter model
     child_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=25)
     age = models.CharField(max_length=25)
     gender = models.CharField(max_length=25)
     babysitters = models.ManyToManyField(Babysitter)
 
+# A model to create a report
 class Report(models.Model):
+    # this model takes 4 inputs, meal, potty, nap, and a foreign key from the Child model
     report_id = models.AutoField(primary_key = True)
     meal = models.CharField(max_length = 255)
     potty = models.CharField(max_length=255)
