a18dc7125b0666cf49a54b915b9177435ff47a9a
Tylerlangsam <90009414+Tylerlangsam@users.noreply.github.com>
2022-01-25 16:15:06 -0800

    Making proposed changes

--- Safehands_Project/Safehands_Project/urls.py
+++ Safehands_Project/Safehands_Project/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('profiles/', include('SafehandsApp.urls'))
+    path('', include('SafehandsApp.urls'))
 ]
